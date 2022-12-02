6af50d739c5f22272c7217a9f870728e5f1cd926
Александр Чехонин <2007cheh@mail.ru>
2022-12-02 11:47:47 +0300

    массив

---
+++ seminar8/Program.cs
@@ -0,0 +1,39 @@
+﻿// Показать двумерный массив размером m x n заполненный целыми числами
+// 1. метод создания массива
+// 2. метод заполнения массива
+// 3. метод печати массива 
+
+int[,]CreateArray(int row, int column)
+{
+    return new int[row, column];
+}
+
+void FillArray(int[,] array)
+{
+    for(int row = 0; row<array.GetLength(0); row++)
+    {
+        for(int column = 0; column < array.GetLength(1); column++)
+        {
+            array[row, column] = new Random().Next(0, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+       for(int column = 0; column < array.GetLength(1); column++)
+       {
+        Console.Write($"{array[row, column], 4}");
+       } 
+       Console.WriteLine();
+    }
+}
+
+
+int[,] matrix = CreateArray(4,5);
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
+
